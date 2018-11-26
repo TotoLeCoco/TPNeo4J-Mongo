b2ac3895bd7df6fd1829b17efe4202bfa57d9b3b
malikToulouse <38778925+malikToulouse@users.noreply.github.com>
2018-11-26 15:41:58 +0100

    modif neo4j

--- src/tpneo4j/mongo/Neo4jClass.java
+++ src/tpneo4j/mongo/Neo4jClass.java
@@ -4,11 +4,43 @@
  * and open the template in the editor.
  */
 package tpneo4j.mongo;
+import java.util.ArrayList;
+import org.neo4j.driver.v1.*;
 
 /**
  *
  * @author Fouad El Ouaryaghli
  */
 public class Neo4jClass {
+    private static Driver driver ;
+    private static Session session ;
+    private static ArrayList<String> listArticle;
+    
+    //connexion Neo4j
+    public static void connexion(){
+        
+        driver = GraphDatabase.driver("bolt://192.168.56.50");
+        session = driver.session();
+    }
+    // Déconnexion Neo4j
+    public static void deconnexion(){
+       
+        session.close();
+        driver.close();
+    }
+    public static void traitementText(int id,String chaine){
+    }
+    // recupere les ids et titres de tous les article 
+    public void listeTitre(){
+        StatementResult result = session.run("MATCH (a:Article) RETURN id(a),a.titre order by id(a) asc");
+        while(result.hasNext()){
+                // lecture de la ligne suivante 
+                Record record = result.next();
+                String ch = record.get("id(a)").asInt()+" - "+ record.get("a.titre").asString();
+                listArticle.add(ch);
+        }
+    }
+    
+    
     
 }
